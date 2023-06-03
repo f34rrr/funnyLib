a3899c042a2561e858b449911220968c7043bff9
funnyboy209j <134348761+funnyboy209j@users.noreply.github.com>
2023-06-03 18:45:40 -0500

    Update hammer.lua

--- hammer.lua
+++ hammer.lua
@@ -1,3 +1,35 @@
+local selectionbox = Instance.new("SelectionBox", workspace)
+local equipped = false
+local oldmouse = mouse.Icon
+local destroytool = Instance.new("Tool", Players.LocalPlayer:FindFirstChildOfClass("Backpack"))
+destroytool.RequiresHandle = false
+destroytool.Name = "Delete"
+destroytool.ToolTip = "from Harked Reborn V2"
+destroytool.TextureId = "http://www.roblox.com/asset/?id=12223874"
+destroytool.CanBeDropped = false
+destroytool.Equipped:connect(function()
+	equipped = true
+	mouse.Icon = "rbxasset://textures\\HammerCursor.png"
+	while equipped do
+		selectionbox.Adornee = mouse.Target
+		wait()
+	end
+end)
+destroytool.Unequipped:connect(function()
+	equipped = false
+	selectionbox.Adornee = nil
+	mouse.Icon = oldmouse
+	print(oldmouse)
+end)
+destroytool.Activated:connect(function()
+	local explosion = Instance.new("Explosion", workspace)
+	explosion.BlastPressure = 0
+	explosion.BlastRadius = 0
+	explosion.DestroyJointRadiusPercent = 0
+	explosion.ExplosionType = Enum.ExplosionType.NoCraters
+	explosion.Position = mouse.Target.Position
+	Destroy(mouse.Target)
+end)
 game:GetService("StarterGui"):SetCoreGuiEnabled('Backpack', true)
 Players.LocalPlayer.CharacterAdded:connect(function()
 	local destroytool = Instance.new("Tool", Players.LocalPlayer:FindFirstChildOfClass("Backpack"))
@@ -30,4 +62,4 @@ Players.LocalPlayer.CharacterAdded:connect(function()
 		Destroy(mouse.Target)
 	end)
 	game:GetService("StarterGui"):SetCoreGuiEnabled('Backpack', true)
-  end)
+end)
